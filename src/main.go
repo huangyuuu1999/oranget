2959c6d7613947fa91b4ac0c4e8f00581ef97cdf
gongyulei <gongyulei@qianxin.com>
2023-01-31 19:46:38 +0800

    fix, comma need in map

--- src/main.go
+++ src/main.go
@@ -13,7 +13,7 @@ func main(){
 			"message": "pong",
 			"name": "gin-webframework",
 			"usage": "backend for orangear",
-			"time": now.Format("2006-01-02 15:04:05.000 Mon Jan")
+			"time": now.Format("2006-01-02 15:04:05.000 Mon Jan"),
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
