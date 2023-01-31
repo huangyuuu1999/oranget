cab2ccc037066761e7d8d38ba03cab793f68acfa
gongyulei <gongyulei@qianxin.com>
2023-01-31 18:07:30 +0800

    test ci by simple time pkg

--- src/main.go
+++ src/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"time"
 	"github.com/gin-gonic/gin"
 )
 
 func main(){
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		now := time.Now()
 		c.JSON(200, gin.H{
 			"message": "pong",
 			"name": "gin-webframework",
 			"usage": "backend for orangear",
+			"time": now.Format("2006-01-02 15:04:05.000 Mon Jan")
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
